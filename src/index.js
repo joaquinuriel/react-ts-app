6c5de59308f5469c527ca82053043eab2d7ceec1
Joaa <54916596+Joaquinuriel@users.noreply.github.com>
2021-06-29 15:59:28 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+var { body } = document;
+
+ReactDOM.render(<App />, body);
---
+++ src/ts/index.ts
@@ -0,0 +1,5 @@
+// import {  } from "react";
+import DOM from "react-dom";
+import App from "../App";
+var {body } = document
+DOM.render(<App/>, body)
